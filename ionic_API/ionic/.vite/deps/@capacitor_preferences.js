e9f004236a8933a98eae0a6184ad8141c65e089e
GillesNC <linggilles@gmail.com>
2026-03-20 15:47:56 +0100

    Registration and unRegistration WIP

---
+++ ionic_API/ionic/.vite/deps/@capacitor_preferences.js
@@ -0,0 +1,7 @@
+import { n as registerPlugin } from "./dist-DwISG4--.js";
+//#region node_modules/@capacitor/preferences/dist/esm/index.js
+var Preferences = registerPlugin("Preferences", { web: () => import("./web-BIHT5elU.js").then((m) => new m.PreferencesWeb()) });
+//#endregion
+export { Preferences };
+
+//# sourceMappingURL=@capacitor_preferences.js.map
---
+++ sf_API/symfony/src/Controller/RegistrationController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\Registration;
+use App\Repository\ActivityRepository;
+use App\Repository\RegistrationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/registration')]
+final class RegistrationController extends AbstractController
+{
+    #[Route('/{activityId}', name: 'app_registration', methods: ['POST'])]
+    public function register(ActivityRepository $activityRepository, EntityManagerInterface $entityManager, int $activityId): JsonResponse
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return new JsonResponse(['message' => 'Utilisateur non authentifié'], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+
+        $activity = $activityRepository->find($activityId);
+        if (!$activity) {
+            return new JsonResponse(['message' => 'Activité non trouvée'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        if ($activity->getUser() === $user) {
+            return new JsonResponse(['error' => 'Vous ne pouvez pas vous inscrire à votre propre activité'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        $registration = new Registration();
+        $registration->setUser($user);
+        $registration->setActivity($activity);
+
+        $entityManager->persist($registration);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Inscription réussie'], JsonResponse::HTTP_CREATED);
+    }
+
+    #[Route('/delete/{id}', name: 'app_unregistration', methods: ['DELETE'])]
+    public function unregister(RegistrationRepository $registrationRepository, EntityManagerInterface $entityManager, int $id): JsonResponse
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return new JsonResponse(['message' => 'Utilisateur non authentifié'], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+
+        $registrationActivity = $registrationRepository->find($id);
+        if (!$registrationActivity) {
+            return new JsonResponse(['message' => 'Inscription non trouvée'], JsonResponse::HTTP_NOT_FOUND);
+        }
+        //dd($registrationActivity);
+
+        $entityManager->remove($registrationActivity);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Désinscription réussie'], JsonResponse::HTTP_OK);
+    }
+}
